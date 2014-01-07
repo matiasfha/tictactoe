e80e7065bd84c2c684eef9211e1db71f7d0a7e28
Matias Hernandez <matias.hernandez@a-dedo.cl>
2014-01-07 12:52:39 -0300

    fix assets paths

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,8 @@ Tictactoe::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+
+  config.assets.paths << Rails.root.join('app','assets','javascripts','templates')
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
@@ -59,7 +61,7 @@ Tictactoe::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( .html )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
